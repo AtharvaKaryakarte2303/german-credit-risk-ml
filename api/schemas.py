45d8cb40bc8397b91193f98961eca9c4b628a2c9
AtharvaKaryakarte2303 <akaryakarte12@gmail.com>
2025-10-27 17:15:54 +0530

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -4,9 +4,12 @@ class CreditData(BaseModel):
     duration: float
     credit_amount: float
     age: float
-    checking_account: float
-    savings_account: float
-    employment_since: float
-    installment_rate: float
-    housing: float
-    job: float
+    checking_status_A11: int
+    checking_status_A12: int
+    checking_status_A13: int
+    savings_status_A61: int
+    savings_status_A62: int
+    savings_status_A63: int
+    employment_A71: int
+    employment_A72: int
+    employment_A73: int
