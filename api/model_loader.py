66defbe4c7ca5309ce10e6533fc0cc30cdb39c41
AtharvaKaryakarte2303 <akaryakarte12@gmail.com>
2025-10-29 18:52:55 +0530

    Update model_loader.py

--- api/model_loader.py
+++ api/model_loader.py
@@ -9,7 +9,7 @@ def load_model():
     # Model paths
     model_path = os.path.join(models_dir, "xgb.joblib")
     scaler_path = os.path.join(models_dir, "scaler.joblib")
-    encoder_path = os.path.join(base_path, "models", "label_encoders.joblib")
+    encoder_path = os.path.join(models_dir, "label_encoders.joblib")
 
     # Load model and scaler
     model = joblib.load(model_path)
