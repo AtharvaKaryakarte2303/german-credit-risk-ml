26158feabe092563dba9a17984936102fa14a2c1
AtharvaKaryakarte2303 <akaryakarte12@gmail.com>
2025-10-30 17:02:33 +0530

    Update schemas.py

--- api/schemas.py
+++ api/schemas.py
@@ -1,23 +1,23 @@
 from pydantic import BaseModel
 
 class CreditData(BaseModel):
-    duration_in_month: int
-    credit_amount: int
-    installment_rate: int
-    age: int
-    existing_credits: int
-    num_dependents: int
     checking_account_status: str
-    savings_account_status: str
+    duration_in_month: int
     credit_history: str
     purpose: str
+    credit_amount: int
+    savings_account_status: str
     employment: str
+    installment_rate: int
     personal_status: str
     other_debtors: str
+    present_residence_since: int
     property: str
+    age: int
     other_installment_plans: str
     housing: str
+    existing_credits: int
     job: str
+    num_dependents: int
     telephone: str
     foreign_worker: str
-    present_residence_since: int
